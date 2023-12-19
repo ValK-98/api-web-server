4101d6242a7892f8e071e99c0d1d9dbc3fb74776
ValK-98 <vkornijenka@gmail.com>
2023-12-19 19:46:06 +1000

    added smartwatches_bp comments

--- src/blueprints/smartwatches_bp.py
+++ src/blueprints/smartwatches_bp.py
@@ -1,3 +1,4 @@
+# Importing necessary modules and models
 from flask import Blueprint, request, jsonify, abort
 from app import db
 from blueprints.auth_bp import admin_required
@@ -6,6 +7,7 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from marshmallow.exceptions import ValidationError
 from models.user import User 
 
+# Creating a Blueprint for smartwatch related routes
 smartwatch_bp = Blueprint('smartwatches', __name__)
 smartwatch_schema = SmartwatchSchema()
 smartwatches_schema = SmartwatchSchema(many=True)
@@ -14,6 +16,7 @@ smartwatches_schema = SmartwatchSchema(many=True)
 @smartwatch_bp.route('/smartwatches', methods=['GET'])
 @jwt_required()
 def get_smartwatches():
+    # Retrieving filter parameters from the request
     query = Smartwatch.query
     budget = request.args.get('budget')
     battery_life = request.args.get('battery_life')
@@ -33,26 +36,37 @@ def get_smartwatches():
     if brand:
         query = query.filter(Smartwatch.brand.ilike(f'%{brand}%'))
 
+    # Handling invalid year format in the filter
     try:
         if year_released:
             query = query.filter(Smartwatch.year_released == int(year_released))
     except ValueError:
         return jsonify({"error": "Invalid year format"}), 400
 
+    # Executing the query and returning the results
     smartwatches = query.all()
     return jsonify(smartwatches_schema.dump(smartwatches)), 200
 
 @smartwatch_bp.route('/smartwatches/<int:model_id>', methods=['GET'])
 @jwt_required()
 def get_smartwatch(model_id):
+    """
+    Endpoint to retrieve a specific smartwatch by its ID.
+    Requires JWT authentication.
+    """
     smartwatch = Smartwatch.query.get_or_404(model_id)
     return jsonify(smartwatch_schema.dump(smartwatch)), 200
 
 @smartwatch_bp.route('/smartwatches', methods=['POST'])
 @jwt_required()
 def create_smartwatch():
+    """
+    Endpoint to create a new smartwatch.
+    Requires JWT authentication and admin privileges.
+    """
     admin_required()
     try:
+        # Loading and validating smartwatch data from the request
         smartwatch_data = smartwatch_schema.load(request.json)
         new_smartwatch = Smartwatch(**smartwatch_data)
         db.session.add(new_smartwatch)
@@ -64,9 +78,14 @@ def create_smartwatch():
 @smartwatch_bp.route('/smartwatches/<int:model_id>', methods=['PUT'])
 @jwt_required()
 def update_smartwatch(model_id):
+    """
+    Endpoint to update an existing smartwatch by its ID.
+    Requires JWT authentication and admin privileges.
+    """
     admin_required()
     smartwatch = Smartwatch.query.get_or_404(model_id)
     try:
+        # Loading and validating updated smartwatch data from the request
         updated_data = smartwatch_schema.load(request.json)
         for key, value in updated_data.items():
             setattr(smartwatch, key, value)
@@ -78,6 +97,10 @@ def update_smartwatch(model_id):
 @smartwatch_bp.route('/smartwatches/<int:model_id>', methods=['DELETE'])
 @jwt_required()
 def delete_smartwatch(model_id):
+    """
+    Endpoint to delete a smartwatch by its ID.
+    Requires JWT authentication and admin privileges.
+    """
     admin_required()
     smartwatch = Smartwatch.query.get_or_404(model_id)
     db.session.delete(smartwatch)
