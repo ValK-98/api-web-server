78faab980d306f594c3ef9725717541647c2018c
ValK-98 <vkornijenka@gmail.com>
2023-12-19 19:48:14 +1000

    added user_smartwatches comments

--- src/blueprints/user_smartwatches_bp.py
+++ src/blueprints/user_smartwatches_bp.py
@@ -1,3 +1,4 @@
+# Importing necessary modules and models
 from flask import Blueprint, request, jsonify
 from app import db
 from models.user_smartwatch import UserSmartwatch, UserSmartwatchSchema
@@ -8,21 +9,29 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from blueprints.auth_bp import admin_required
 from sqlalchemy import and_ 
 
+# Creating a Blueprint for user smartwatch related routes
 user_smartwatches_bp = Blueprint('user_smartwatches', __name__)
 
 @user_smartwatches_bp.route('/user_smartwatches', methods=['POST'])
 @jwt_required()
 def add_smartwatch_to_user():
+    """
+    Endpoint to add a smartwatch to a user's collection.
+    Requires JWT authentication.
+    """
     user_id = get_jwt_identity()
 
+    # Validate the request data against SmartwatchSchema
     try:
         SmartwatchSchema().load(request.json)  
     except ValidationError as err:
         return jsonify(err.messages), 400
     
+    # Build a query to find a smartwatch based on the provided filters
     query = Smartwatch.query
     for attr in ['budget', 'battery_life', 'main_feature', 'name', 'brand', 'year_released']:
         if value := request.json.get(attr):
+            # Apply filters to the query based on attribute type
             if attr in ['name', 'brand', 'main_feature']:
                 query = query.filter(getattr(Smartwatch, attr).ilike(f'%{value}%'))
             elif attr == 'year_released':
@@ -30,13 +39,16 @@ def add_smartwatch_to_user():
             else:
                 query = query.filter(getattr(Smartwatch, attr) == value)
 
+    # Execute the query and check if the smartwatch exists
     smartwatch = query.first()
     if not smartwatch:
         return jsonify({"error": "Smartwatch not found"}), 404
 
+    # Check if the smartwatch is already in the user's collection
     if UserSmartwatch.query.filter(and_(UserSmartwatch.user_id == user_id, UserSmartwatch.smartwatch_id == smartwatch.id)).first():
         return jsonify({"error": "Smartwatch already added to user's collection"}), 400
 
+    # Add the smartwatch to the user's collection and commit the transaction
     user_smartwatch = UserSmartwatch(user_id=user_id, smartwatch_id=smartwatch.id)
     db.session.add(user_smartwatch)
     db.session.commit()
@@ -45,6 +57,9 @@ def add_smartwatch_to_user():
 
 @jwt_required()
 def get_smartwatch_data(user_smartwatches):
+    """
+    Retrieves detailed data of smartwatches for the given user smartwatches.
+    """
     smartwatches = []
     for user_smartwatch in user_smartwatches:
         smartwatch = Smartwatch.query.get(user_smartwatch.smartwatch_id)
@@ -62,6 +77,10 @@ def get_smartwatch_data(user_smartwatches):
 @user_smartwatches_bp.route('/user_smartwatches', methods=['GET'])
 @jwt_required()
 def get_user_smartwatches():
+    """
+    Endpoint to get the smartwatches of the currently authenticated user.
+    Requires JWT authentication.
+    """
     user_id = get_jwt_identity()
     user_smartwatches = UserSmartwatch.query.filter_by(user_id=user_id).all()
     smartwatches = get_smartwatch_data(user_smartwatches)
@@ -70,6 +89,10 @@ def get_user_smartwatches():
 @user_smartwatches_bp.route('/user_smartwatches/users/all', methods=['GET'])
 @jwt_required()
 def get_all_users_smartwatches():
+    """
+    Endpoint to get smartwatches of all users. 
+    Requires JWT authentication and admin privileges.
+    """
     admin_required()
     user_smartwatches = UserSmartwatch.query.all()
     smartwatches = get_smartwatch_data(user_smartwatches)
@@ -78,6 +101,10 @@ def get_all_users_smartwatches():
 @user_smartwatches_bp.route('/user_smartwatches/users/<int:user_id>', methods=['GET'])
 @jwt_required()
 def get_specific_user_smartwatches(user_id):
+    """
+    Endpoint to get the smartwatches of a specific user.
+    Requires JWT authentication and admin privileges.
+    """
     admin_required()
     user_smartwatches = UserSmartwatch.query.filter_by(user_id=user_id).all()
     smartwatches = get_smartwatch_data(user_smartwatches)
@@ -86,12 +113,18 @@ def get_specific_user_smartwatches(user_id):
 @user_smartwatches_bp.route('/user_smartwatches/<int:smartwatch_id>', methods=['DELETE'])
 @jwt_required()
 def delete_user_smartwatch(smartwatch_id):
+    """
+    Endpoint to remove a smartwatch from the currently authenticated user's collection.
+    Requires JWT authentication.
+    """
     user_id = get_jwt_identity()
     user_smartwatch = UserSmartwatch.query.filter_by(user_id=user_id, smartwatch_id=smartwatch_id).first()
 
+    # Check if the smartwatch exists in the user's collection
     if not user_smartwatch:
         return jsonify({"error": "Smartwatch not found in user's collection"}), 404
 
+    # Remove the smartwatch from the user's collection and commit the transaction
     db.session.delete(user_smartwatch)
     db.session.commit()
 
